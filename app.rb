8ddc98a58c921757bb32767ea4bda6a9e3dd6411
ayaka9922 <ayk.24a3y@gmail.com>
2020-02-09 04:58:03 +0000

    completed quiz

--- app.rb
+++ app.rb
@@ -6,5 +6,43 @@ require './models.rb'
 before do
     if Result.all.length == 0
         Result.create(score:0)
+end
+
+@questions = Array.new
+@questions.push(["Webサービスプログラミングコースを学んでいる、赤色の宝石の名前を使っている言語は？","Garnet","Ruby","Amethyst","Opal","2"])
+@questions.push(["life is Tech!でプログラミングを教えてくれるのは誰？","メンバー","センセー","メンター","センサー","3"])
+end
+
+get '/' do
+    result = Result.first
+    result.score = 0
+    result.save
+    redirect '/question/0'
+end
+
+get '/question/:id' do
+    @number = params[:id].to_i
+    erb :question
+end
+
+post '/question/check/:id' do
+    number = params[:id].to_i
+    answer = @questions[number][5]
+    select_answer = params[:select_answer]
+    if answer == select_answer
+        result = Result.first
+        result.score = result.score + 1
+        result.save
     end
+    if number + 1 < @questions.length
+        number = number + 1
+        redirect "/question/#{number}"
+    else
+        redirect "/result"
+    end
+end
+
+get '/result' do
+    @score = Result.first.score
+    erb :result
 end
